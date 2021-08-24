aca6e999570b70c1841e530b4bb4b18877308607
Alexey Lutin <lutin@smedia.ru>
2021-08-24 19:43:30 +0300

    added ansible inventory and check ping-pong

--- init_ssh.sh
+++ init_ssh.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-docker-compose exec -T admin_host bash -c "ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa"
-docker-compose exec -T admin_host bash -c "cat /root/.ssh/id_rsa.pub > /var/ssh_pub/id_rsa.pub"
+#docker-compose exec -T --user root admin_host bash -c "ssh-keygen -q -t rsa -N '' -f /home/admin/.ssh/id_rsa"
+docker-compose exec -T --user root admin_host bash -c "cat /home/admin/.ssh/id_rsa.pub > /var/ssh_pub/id_rsa.pub \
+    && chmod 400 /home/admin/.ssh/id_rsa*"
 docker-compose exec -T client_host_01 bash -c "cat /var/ssh_pub/id_rsa.pub >> /home/client_host/.ssh/authorized_keys"
 docker-compose exec -T client_host_02 bash -c "cat /var/ssh_pub/id_rsa.pub >> /home/client_host/.ssh/authorized_keys"
 docker-compose exec -T client_host_03 bash -c "cat /var/ssh_pub/id_rsa.pub >> /home/client_host/.ssh/authorized_keys"
+
+echo -e "\e[1;34m SSH configs has been created\e[0m"
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 docker-compose up -d
 chmod +x ./init_ssh.sh
 ./init_ssh.sh
+
+docker-compose exec --user admin admin_host bash
+cd /home/admin/ansible
+ansible all -m ping
